cb181a922afa8c4fffde966aebd2fcf73bb62070
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-06 15:53:51 +0900

    [Add] SoundSettingActivity(音声設定画面)を追加。

---
+++ app/src/main/java/com/example/miura/notificationtimer/SoundSettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.miura.notificationtimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SoundSettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sound_setting)
+    }
+}
