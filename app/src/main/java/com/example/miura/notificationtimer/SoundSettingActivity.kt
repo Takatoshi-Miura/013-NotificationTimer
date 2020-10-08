e2566a1ad448497ee12c6579c4ac6fd38e6adb0a
Takatoshi Miura <takatoshi052031@gmail.com>
2020-10-08 16:57:27 +0900

    [Update] SoundSettingActivityにて、セクションの項目を追加。

--- app/src/main/java/com/example/miura/notificationtimer/SoundSettingActivity.kt
+++ app/src/main/java/com/example/miura/notificationtimer/SoundSettingActivity.kt
@@ -25,18 +25,20 @@ class SoundSettingActivity : AppCompatActivity() {
     }
 
 
-
     // 音声設定リストをロードするメソッド
     private fun loadFolders() {
         // 宣言
         val folders = mutableListOf<Map<String, String>>()
         val categoriesOfFolder = mutableListOf<MutableList<Map<String, String>>>()
 
-
         // タイトルを格納
-        var folderTitleList   = arrayOf("一括設定","終了時","経過時間通知","残り時間通知","その他")
-        var categoryTitleList = listOf("マナーモード","")
-
+        var folderTitleList    = arrayOf("一括設定","終了時","経過時間通知","残り時間通知","その他")
+        var categoryTitleList0 = arrayOf("マナーモード")
+        var categoryTitleList1 = arrayOf("終了時")
+        var categoryTitleList2 = arrayOf("5分経過","10分経過","15分経過","20分経過","25分経過",
+                                         "30分経過","35分経過","40分経過","45分経過","50分経過")
+        var categoryTitleList3 = arrayOf("残り30秒","残り1分","残り3分","残り5分")
+        var categoryTitleList4 = arrayOf("タイマー起動時")
 
         // リストの項目を作成
         for (i in 0..4) {
@@ -46,11 +48,28 @@ class SoundSettingActivity : AppCompatActivity() {
 
             // セクションの項目を作成
             val categories = mutableListOf<Map<String, String>>()
-            for (j in 1..3) {
-                val todo = mapOf(KEY_CHILD to KEY_CHILD + j.toString())
-                categories.add(todo)
+            when (i) {
+                0 -> for (j in 0..categoryTitleList0.size - 1) {
+                        val title = mapOf(KEY_CHILD to categoryTitleList0[j])
+                        categories.add(title)
+                     }
+                1 -> for (j in 0..categoryTitleList1.size - 1) {
+                        val title = mapOf(KEY_CHILD to categoryTitleList1[j])
+                        categories.add(title)
+                     }
+                2 -> for (j in 0..categoryTitleList2.size - 1) {
+                        val title = mapOf(KEY_CHILD to categoryTitleList2[j])
+                        categories.add(title)
+                     }
+                3 -> for (j in 0..categoryTitleList3.size - 1) {
+                        val title = mapOf(KEY_CHILD to categoryTitleList3[j])
+                        categories.add(title)
+                     }
+                4 -> for (j in 0..categoryTitleList4.size - 1) {
+                        val title = mapOf(KEY_CHILD to categoryTitleList4[j])
+                        categories.add(title)
+                     }
             }
-
             categoriesOfFolder.add(categories)
         }
 
